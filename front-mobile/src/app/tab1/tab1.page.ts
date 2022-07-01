f6cbf2d03ac32fffbd5381cecac84c535606f145
Orkatz ARANO <orkatz@studiowaaz.com>
2022-07-01 19:45:32 +0200

    Fetch datas API fake + affichage

--- front-mobile/src/app/tab1/tab1.page.ts
+++ front-mobile/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,18 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  data: any[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http
+      .get(`https://dummyjson.com/products?limit=10`)
+      .subscribe((resp: { products: any }) => {
+        console.log(resp.products)
+        this.data = resp.products
+      });
+  }
 
 }
