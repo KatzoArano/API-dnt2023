d4c8fb71a2fa9af6dc0203e58090521b7f5d15cc
orkatz <orkatz@wanadoo.fr>
2022-11-09 11:09:14 +0100

    List User en progression

--- front/src/app/admin/user/u-index/u-index.component.ts
+++ front/src/app/admin/user/u-index/u-index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-u-index',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./u-index.component.css']
 })
 export class UIndexComponent implements OnInit {
+  userList: any[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get('http://localhost:8888/users').subscribe(
+      (data: any) => {
+        this.userList = data.users;
+      }
+    )
   }
 
 }
--- front/src/app/auth/login/login.component.ts
+++ front/src/app/auth/login/login.component.ts
@@ -1,6 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ICredential } from 'src/app/_interfaces/credential';
+import { IToken } from 'src/app/_interfaces/token';
+import { AuthService } from 'src/app/_services/auth.service';
+// import { TokenService } from 'src/app/_services/token.service';
+
 
 @Component({
   selector: 'app-login',
@@ -10,19 +14,35 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
 
 
-  form: any = {
-    email: null,
-    password: null
+  form: ICredential = {
+    email: '',
+    password: ''
   }
-  constructor(private http: HttpClient, private router: Router) { }
+
+  constructor(
+    // private authService: AuthService,
+    // private tokenService: TokenService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
-  login() {
-    if (this.form.email === "orkatz@wanadoo.fr" && this.form.password === "orkatz") {
-      this.router.navigate(['home']);
-    }
-  }
+  // onSubmit(): void {
+  //   console.log(this.form)
+  //   this.authService.login(this.form).subscribe(
+  //     data => {
+  //       console.log(data.access_token)
+  //       this.tokenService.saveToken(data.access_token)
+  //     },
+  //     err => console.log(err)
+  //   )
+  // }
+
+  // login() {
+  //   if (this.form.email === "orkatz@wanadoo.fr" && this.form.password === "orkatz") {
+  //     this.router.navigate(['home']);
+  //   }
+  // }
 
 }
