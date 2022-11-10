9a010adde53cc6abf31c0959c5ced856261a71c7
orkatz <orkatz@wanadoo.fr>
2022-11-10 12:34:11 +0100

    Ionic: Page produits wip

--- front-mobile/src/app/tab3/tab3.page.ts
+++ front-mobile/src/app/tab3/tab3.page.ts
@@ -7,6 +7,58 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  produitList: any[] = [];
+
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+
+    this.produitList = [
+      {
+        "nom": "Paella",
+        "description": "Ceci est la description de la paella",
+        "categorie": "Plat",
+        "prix": "12€",
+        "disponible": true
+      },
+      {
+        "nom": "Blanc poulet, riz",
+        "description": "Ceci est la description du blanc boulet riz",
+        "categorie": "Plat",
+        "prix": "12€",
+        "disponible": true
+      },
+      {
+        "nom": "Burger",
+        "description": "Ceci est la description du burger",
+        "categorie": "Plat",
+        "prix": "12€",
+        "disponible": false
+      },
+      {
+        "nom": "Croque monsieur",
+        "description": "Ceci est la description du croque monsieur",
+        "categorie": "Plat",
+        "prix": "12€",
+        "disponible": false
+      },
+      {
+        "nom": "Pizza",
+        "description": "Ceci est la description de la pizza",
+        "categorie": "Plat",
+        "prix": "12€",
+        "disponible": false
+      },
+      {
+        "nom": "Fish and potatoes",
+        "description": "Ceci est la description du fish and potatoes maison",
+        "categorie": "Plat",
+        "prix": "12€",
+        "disponible": true
+      },
+    ]
+  }
 
 }
